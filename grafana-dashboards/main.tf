16d73314166c869e41c87e808135930feac3e2c0
haroldsphinx <adedayoakinpelu@gmail.com>
2023-06-20 00:58:45 +0100

    Setup Terraform to boostratp creation of Grafana Dashboard
    Signed-off-by: haroldsphinx <adedayoakinpelu@gmail.com>

--- grafana-dashboards/main.tf
+++ grafana-dashboards/main.tf
@@ -11,25 +11,10 @@ locals {
   dashboard_set = toset(var.dashboards)
 }
 
-resource "random_string" "random" {
-  for_each = local.dashboard_set
-  length   = 12
-  special  = false
-}
-
 resource "grafana_folder" "env_folder" {
   title = var.folder_name
 }
 
-locals {
-  dashboard_set = toset(var.dashboards)
-}
-
-resource "random_string" "random" {
-  for_each = local.dashboard_set
-  length   = 12
-  special  = false
-}
 
 resource "grafana_dashboard" "dashboard" {
   folder    = grafana_folder.env_folder.id
